dd34c7582247b254424878e1937fc75fd6f0c2d4
Edward Karogo <prezokams@gmail.com>
2025-06-12 06:52:10 +0300

    Functions Lab continued

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(todo = "go to the office") {
+  return `This Monday, I will ${todo}.`
+}
+function wrapAdjective(visualFlair = "*") {
+  return function innerFxn(myDefault = "special") {
+    return `You are ${visualFlair}${myDefault}${visualFlair}!`
+  };
+}
